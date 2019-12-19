c051cd989a2cbbca65d483a2490bce9b69e276a5
Roshanak Agahi <rosh.agahi@gmail.com>
2019-12-19 20:53:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,15 +61,16 @@ class Dog
   end
   
   def self.find_by_name(name)
-    name_query = <<-SQL 
-    SELECT * FROM dogs 
-    WHERE name = ?
-    li
-    SQL
-    DB[:conn].execute(id_query,id).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
+   name_query = <<-SQL
+   SELECT * FROM dogs
+   WHERE name = ?
+   LIMIT 1
+   SQL
+   
+   DB[:conn].execute(name_query, name).map do |row|
+     self.new_from_db(row)
+   end.first
+ end
 
   def self.find_or_create_by(name:, breed:)
    sql = <<-SQL
